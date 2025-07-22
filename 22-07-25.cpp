b5b886ac94a8bf1df4ae8c95ef197715901f3949
Santoshabhi <159675295+Santoshabhi@users.noreply.github.com>
2025-07-22 20:21:48 +0530

    Update and rename 22-07-25 to 22-07-25.cpp

--- 22-07-25.cpp
+++ 22-07-25.cpp
@@ -13,3 +13,24 @@ int main() {
 
     return 0;
 }
+
+
+//count number
+include<iostream>
+using namespace std;
+
+int main()
+{
+int n;
+cin>>n;
+int c=0;
+while(n>0){
+int l=n%10;
+c++;
+n=n/10;
+
+}
+cout<<c;
+    
+    return 0;
+}
